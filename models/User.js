7f43222d0927076dad69326c36f0821495239527
byio <ben@benyang.io>
2018-03-05 16:58:46 +0800

    create User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Define User Schema
+const UserSchema = new Schema({
+  googleID: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  image: {
+    type: String
+  }
+});
+
+// Create User Model from User Schema, and Export Module
+module.exports = mongoose.model('users', UserSchema);
