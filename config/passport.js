b74c4e9494e6b1701cdad769bb8fdae17eec30a5
byio <ben@benyang.io>
2018-03-05 23:33:04 +0800

    create new user if email is unique in db

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const User = require('../models/User');
 const keys = require('./keys');
 
 module.exports = (passport) => {
@@ -10,8 +11,29 @@ module.exports = (passport) => {
       callbackURL: '/auth/google/callback',
       proxy: true
     }, (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(profile);
+      // check if user exists in db
+      User.findOne({ googleID: profile.id })
+          .then(user => {
+            // case: user exists
+            if (user) {
+              done(null, user);
+            } else {
+              // case: new user
+              const imageURL = profile.photos[0].value;
+              const image = imageURL.substring(0, imageURL.indexOf('?'));
+              const newUser = new User({
+                googleID: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                image
+              });
+              newUser.save()
+                     .then(user => {
+                       done(null, user)
+                     });
+            }
+          });
     })
   );
 };
