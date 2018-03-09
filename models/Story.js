5725ed98112bb31efa0c63e7f4a2cf0277307078
byio <ben@benyang.io>
2018-03-09 19:13:56 +0800

    create Story model

---
+++ models/Story.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Define Story Schema
+const StorySchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    default: 'public'
+  },
+  allowComments: {
+    type: Boolean,
+    default: true
+  },
+  comments: [{
+    commentBody: {
+      type: String,
+      required: true
+    },
+    commentDate: {
+      type: Date,
+      default: Date.now()
+    },
+    commentUser: {
+      type: Schema.Types.ObjectId,
+      refs: 'users'
+    }
+  }],
+  user: {
+    type: Schema.Types.ObjectId,
+    refs: 'users'
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+// Create Story Model from Story Schema, and Exprot Module
+module.exports = mongoose.model('Story', StorySchema);
