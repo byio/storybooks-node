410f6a15d2874b49fd8ae107943a330ed52f2912
byio <ben@benyang.io>
2018-03-09 20:10:48 +0800

    save new story from add story form

--- controllers/storiesController.js
+++ controllers/storiesController.js
@@ -1,12 +1,31 @@
+// Load Story Model
+const Story = require('../models/Story');
+
 exports.renderAllStories = (req, res) => {};
 
-exports.renderOneStory = (req, res) => {};
+exports.renderOneStory = (req, res) => {
+  res.send(`showing story with id ${req.params.id}`);
+};
 
 exports.renderAddStoryForm = (req, res) => {
   res.render('stories/add');
 };
 
-exports.addStory = (req, res) => {};
+exports.addStory = (req, res) => {
+  // create new story object
+  const newStory = new Story({
+    title: req.body.title,
+    body: req.body.body,
+    status: req.body.status,
+    allowComments: !!req.body.allowComments,
+    user: req.user.id
+  });
+  // save new story object to db
+  newStory.save()
+          .then(story => {
+            res.redirect(`/stories/show/${story._id}`);
+          });
+};
 
 exports.renderEditStoryForm = (req, res) => {};
 
--- routes/stories.js
+++ routes/stories.js
@@ -8,8 +8,8 @@ const stories_controller = require('../controllers/storiesController');
 // /stories Routes
 router.get('/', stories_controller.renderAllStories);
 router.get('/add', stories_controller.renderAddStoryForm);
-router.post('/add', stories_controller.addStory);
-router.get('/:id', stories_controller.renderOneStory);
+router.post('/', stories_controller.addStory);
+router.get('/show/:id', stories_controller.renderOneStory);
 router.get('/edit/:id', stories_controller.renderEditStoryForm);
 router.put('/:id', stories_controller.updateStory);
 router.delete('/:id', stories_controller.delStory);
