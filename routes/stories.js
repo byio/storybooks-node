33fc1f6877fe85feb2635f41b9a447d34c9ccbe7
byio <ben@benyang.io>
2018-03-11 21:33:26 +0800

    add access control to stories routes

--- routes/stories.js
+++ routes/stories.js
@@ -2,17 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+// Import Helpers
+const { ensureAuthenticated } = require('../helpers/auth');
+
 // Load Controllers
 const stories_controller = require('../controllers/storiesController');
 
 // /stories Routes
 router.get('/', stories_controller.fetchAllStories);
-router.get('/add', stories_controller.renderAddStoryForm);
-router.post('/', stories_controller.addStory);
+router.get('/add', ensureAuthenticated, stories_controller.renderAddStoryForm);
+router.post('/', ensureAuthenticated, stories_controller.addStory);
 router.get('/show/:id', stories_controller.renderOneStory);
-router.get('/edit/:id', stories_controller.renderEditStoryForm);
-router.put('/:id', stories_controller.updateStory);
-router.delete('/:id', stories_controller.delStory);
+router.get('/edit/:id', ensureAuthenticated, stories_controller.renderEditStoryForm);
+router.put('/:id', ensureAuthenticated, stories_controller.updateStory);
+router.delete('/:id', ensureAuthenticated, stories_controller.delStory);
 
 // Export Module
 module.exports = router;
